bae3656034ea699ba873f0d76b4f7247d69e3be4
dulleydoo <karem.jabar2015@gmail.com>
2022-09-07 01:48:51 -0400

    halfway

--- src/main/java/HwMethods.java
+++ src/main/java/HwMethods.java
@@ -1,6 +1,4 @@
 public class HwMethods {
-    String names;
-
 
     //QUESTION 1
     public static int getLastIndex(String[] names) {
@@ -46,22 +44,35 @@ public class HwMethods {
         return sum / ints.length;
 
     }
-//
-//    //QUESTION 8
-//    public static String extractAllOddNumbers(int[] numbers) {
-//        return null;
-//    }
-//
-//    //QUESTION 9
-//    public static String extractAllEvenNumbers(int[] numbers) {
-//        return null;
-//    }
-//
-//    //QUESTION 10
-//    public static boolean contains(String[] names, String element) {
-//        return false;
-//    }
-//
+
+    //QUESTION 8
+    public static int extractAllOddNumbers(int [] numbers) {
+        int odds = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (i % 2 == 0)
+                odds++;
+        }
+        return numbers[odds];
+
+
+    }
+
+
+    //QUESTION 9
+    public static String extractAllEvenNumbers(int [] numbers) {
+        int evens = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (i % 2 != 0)
+                evens++;
+        }
+        return String.valueOf(numbers[evens]);
+    }
+
+    //QUESTION 10
+    public static boolean contains(String[] names, String element) {
+        return false;
+    }
+
 //    //QUESTION 11
 //    public static int getIndexByElement(String[] names, String element) {
 //        return 0;
@@ -82,33 +93,49 @@ public class HwMethods {
 //        return null;
 //    }
 //
-//    //QUESTION 15
-//// Write a java method to count all the words in a string
-//
-//    public static int WordsInAStringCounter(String str) {
-//        //return null;
-//    }
+    //QUESTION 15
+    // Write a java method to count all the words in a string
+    public static int WordsInAStringCounter(String str) {
+        String[] numberOfWords = str.split("\\s+");
+        return numberOfWords.length;
+    }
 
 
 
 
     public static void main(String[] args) {
         String []names = {"Karem", "Omar", "Aqil", "kyle", "Emily"};
-        int []ints = {5, 7, 3 , 5, 10};
+        int [] ints = {5, 7, 3 , 5, 10};
+        int [] numbers ={1,2,3,4,5,6,7,8,9,10};
+        String str = "That way is the best way out";
         //QUESTION 1
-        System.out.println(getLastIndex(names));
+        //System.out.println(getLastIndex(names));
         //QUESTION 2
-        System.out.println(getSecondToLastIndex(names));
+       // System.out.println(getSecondToLastIndex(names));
         //QUESTION 3
-        System.out.println(getFirstElement(names));
+       // System.out.println(getFirstElement(names));
         //QUESTION 4
-        System.out.println(getLastElement(names));
+       // System.out.println(getLastElement(names));
         //QUESTION 5
-        System.out.println(getSecondToLastElement(names));
+       // System.out.println(getSecondToLastElement(names));
         //QUESTIONS 6
-        System.out.println(getSum(ints));
+       // System.out.println(getSum(ints));
         //QUESTIONS 7
-        System.out.println(getAverage(ints));
+       // System.out.println(getAverage(ints));
+        //QUESTION 8
+        //System.out.println(extractAllOddNumbers());
+        //QUESTION 9
+        //System.out.println(extractAllEvenNumbers());
+        //QUESTION 10
+        //QUESTION 11
+        //QUESTION 12
+        //QUESTION 13
+        //QUESTION 14
+        //QUESTION 15
+        //System.out.println(WordsInAStringCounter(str));
+
+
+
 
 
 
